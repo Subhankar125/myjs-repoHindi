a96687d946dd8c91c475a5e4ff4ad00e951be819
SubhankarS <96858060+Subhankar125@users.noreply.github.com>
2025-03-14 18:45:34 +0000

    dataType

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,20 @@
+"use strict";  //treat all JS code as newer  version
+//alert(3+3) //we are using nodejs not browser
+ 
+
+let name = "Subha"
+let age = 30
+let isLoggedIn = false
+
+//======data types:=============
+//number => 2^53
+//bigint => biger then above range of number
+//boolean => true/false
+//null => standalone value (means not undefine but it has no value)
+//undefine => veriable decleared but value not defined
+//Symbol => uniqueness
+
+
+//object
+console.log(typeof null) // output: Object
+console.log(typeof undefined)// output; undefined
