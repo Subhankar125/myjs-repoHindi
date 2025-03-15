53bed5cb6c1a959c792d566f48bdf5952ae29594
SubhankarS <96858060+Subhankar125@users.noreply.github.com>
2025-03-15 07:37:07 +0000

    conversion_operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,40 @@
+let score = "33sdf" 
+//let score = ture
+
+console.log(typeof score)
+
+let variableInNumber= Number(score)
+console.log(typeof variableInNumber)
+console.log(variableInNumber)
+
+
+let score1 = null
+
+console.log(typeof score)
+
+let variableInNumber1= Number(score1)
+console.log(typeof variableInNumber1)
+console.log(variableInNumber1)
+
+console.log("=====Notes====")
+
+//33 = number
+//33gfd = NaN
+//true = 1; false = 0
+
+// say isloggedin is giving number need to convert ti to boolean
+let isloggedIn = 1
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn)
+
+
+console.log("=====Notes====")
+
+//1 => true ; 0 => false
+//"" => false
+//"subhankar" => true
+
+let someNumber = 30
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
